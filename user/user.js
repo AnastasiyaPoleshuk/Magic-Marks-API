62580fd0db0f9d03160af21f101f2572465dd48d
AnastasiyaPoleshuk <anastasiyapaliashuk@gmail.com>
2022-09-21 19:34:15 +0300

    Create-mock-get-user-endpoint: fix getUser function

--- user/user.js
+++ user/user.js
@@ -2,41 +2,38 @@ const constants = require('../utils/constants')
 
 const getUser = (req, res) => {
   const token = req.body.token;
-  const userId = createUserId(token);
-
-  if (userId) {
-    const userData = createUserData(userId)
-    res.status(200).send(userData);
-  } else {
-    const responseData = { message: "Unauthorized" };
-    res.status(401).send(responseData);
-  }
-
+  const responseData = getUserData(token);
+  res.status(responseData.status).send(responseData.data);
 }
 
-function createUserId(token) {
-  if (token === constants.CONSTANTS.MOCK_TOKEN) {
-    const userId = 1;
-    return userId;
-  } else {
-    return null;
-  }
-}
 
-function createUserData(id) {
-  return {
-    UserId: id,
-    Email: constants.CONSTANTS.MOCK_USER.Email,
-    FirstName: "Светлана",
-    LastName: "Полешук",
-    Class: "1",
-    Subjects: [
-      { SubjectId: 1, SubjectName: "Метематика", AverageMark: 8 },
-      { SubjectId: 2, SubjectName: "Английский", AverageMark: 9 },
-      { SubjectId: 3, SubjectName: "Русский язык", AverageMark: 8 },
-      { SubjectId: 4, SubjectName: "Фмзкультура", AverageMark: 9 },
-      { SubjectId: 5, SubjectName: "ИЗО", AverageMark: 10 },
-    ]
+function getUserData(token) {
+
+  const unauthorized = 401;
+  const ok = 200;
+  const response = {
+    status: ok,
+    data: {}
+  }
+  if (token !== constants.CONSTANTS.MOCK_TOKEN) {
+    response.status = unauthorized;
+    return response;
+  } else {
+    response.data = {
+      UserId: "1",
+      Email: constants.CONSTANTS.MOCK_USER.Email,
+      FirstName: "Светлана",
+      LastName: "Полешук",
+      Class: "1",
+      Subjects: [
+        { SubjectId: 1, SubjectName: "Метематика", AverageMark: 8 },
+        { SubjectId: 2, SubjectName: "Английский", AverageMark: 9 },
+        { SubjectId: 3, SubjectName: "Русский язык", AverageMark: 8 },
+        { SubjectId: 4, SubjectName: "Фмзкультура", AverageMark: 9 },
+        { SubjectId: 5, SubjectName: "ИЗО", AverageMark: 10 },
+      ],
+    }
+    return response;
   }
 };
 

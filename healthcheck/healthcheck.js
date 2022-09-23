0202e32916116a616ee2e882740a7b7ad9bc4a64
AnastasiyaPoleshuk <anastasiyapaliashuk@gmail.com>
2022-09-23 13:12:23 +0300

    API-healthcheck-endpoint-return-wrong-PORT-value: fix bug

--- healthcheck/healthcheck.js
+++ healthcheck/healthcheck.js
@@ -1,20 +1,12 @@
 const constants = require('../utils/constants')
 
 const getHealthStatus = (req) => {
-  const port = getPort(req);
-
   const response = {
-    Port: port,
+    Port: process.env.PORT,
     Application: constants.CONSTANTS.HEALTHCHECK.Application,
     Status: constants.CONSTANTS.HEALTHCHECK.Status
   }
   return response;
-}
-
-function getPort(req) {
-  let hostArr = req.headers.host.split('');
-  const port = hostArr.slice(-4);
-  return port.join('');
 }
 
 module.exports = getHealthStatus;
